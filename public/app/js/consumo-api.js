bef508c74037e77e19b01d1b64884802ce3c8697
caarbelaezg <caarbelaezg@gmail.com>
2020-09-12 17:37:25 -0500

    Remake of Guifos

--- public/app/js/consumo-api.js
+++
@@ -1,9 +0,0 @@
-async function apiConsumption(url) {
-    try {
-        const response = await fetch(url);
-        const jsonResponse = await response.json();
-        return jsonResponse;
-    } catch (error) {
-        console.log('Se presentó un error en el consumo del API' + error)
-    }
-}
--- public/app/js/parameters.js
+++
@@ -1,4 +0,0 @@
-const parameters = {
-    API_BASE_URL: 'http://api.giphy.com/v1/gifs',
-    API_KEY: 'FeH8s28nS47gsfXjis23Brzi6lcQfXOJ'
-}
